2f1e427c13540859136530c823380352f6bb90f4
Gustavo <gustavomacedosousa013@gmail.com>
2024-03-10 13:18:45 -0300

    Adiciona função mostrarDiv e refatora código

--- app.js
+++ app.js
@@ -12,77 +12,51 @@ display: flex;
     flex-direction: column;
     width: 50%;
 */
+function mostrarDiv(div) {
+    if (div.style.display === "flex") {
+        div.style.display = "none";
+    } else {
+        div.style.display = "flex";
+        div.style.flexDirection = "column";
+        div.style.width = "40%";
+    }
+
+}
 var div_1 = document.getElementById('div_botao1');
 botao1.addEventListener('click', function(event) {
     event.preventDefault();
-    if (div_1.style.display === "flex") {
-        div_1.style.display = "none";
-    } else {
-        div_1.style.display = "flex";
-        div_1.style.flexDirection = "column";
-        div_1.style.width = "40%";
-    }
+    mostrarDiv(div_1);
 });
 
 /*BOTÃO 2 */
 var div_2 = document.getElementById('div_botao2');
 botao2.addEventListener('click', function(event) {
     event.preventDefault();
-    if (div_2.style.display === "flex") {
-        div_2.style.display = "none";
-    } else {
-        div_2.style.display = "flex";
-        div_2.style.flexDirection = "column";
-        div_2.style.width = "40%";
-    }
+    mostrarDiv(div_2);
 });
 
 /*BOTÃO 3 */
 var div_3 = document.getElementById('div_botao3');
 botao3.addEventListener('click', function(event) {
     event.preventDefault();
-    if (div_3.style.display === "flex") {
-        div_3.style.display = "none";
-    } else {
-        div_3.style.display = "flex";
-        div_3.style.flexDirection = "column";
-        div_3.style.width = "40%";
-    }
+    mostrarDiv(div_3);
 });
 
 /*BOTÃO 4 */
 var div_4 = document.getElementById('div_botao4');
 botao4.addEventListener('click', function(event) {
     event.preventDefault();
-    if (div_4.style.display === "flex") {
-        div_4.style.display = "none";
-    } else {
-        div_4.style.display = "flex";
-        div_4.style.flexDirection = "column";
-        div_4.style.width = "40%";
-    }
+    mostrarDiv(div_4);
 });
 /*BOTÃO 5 */
 var div_5 = document.getElementById('div_botao5');
 botao5.addEventListener('click', function(event) {
     event.preventDefault();
-    if (div_5.style.display === "flex") {
-        div_5.style.display = "none";
-    } else {
-        div_5.style.display = "flex";
-        div_5.style.flexDirection = "column";
-        div_5.style.width = "40%";
-    }
+    mostrarDiv(div_5);
 });
 /*BOTÃO 6 */
 var div_6 = document.getElementById('div_botao6');
 botao6.addEventListener('click', function(event) {
     event.preventDefault();
-    if (div_6.style.display === "flex") {
-        div_6.style.display = "none";
-    } else {
-        div_6.style.display = "flex";
-        div_6.style.flexDirection = "column";
-        div_6.style.width = "40%";
-    }
+    mostrarDiv(div_6);
 });
