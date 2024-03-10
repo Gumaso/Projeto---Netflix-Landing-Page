25dfc428da04585d31b62eefad686618d1a5f84b
Gustavo <gustavomacedosousa013@gmail.com>
2024-03-10 13:15:04 -0300

    Atualizar eventos de clique nos botões para exibir/ocultar divs

--- app.js
+++ app.js
@@ -13,66 +13,76 @@ display: flex;
     width: 50%;
 */
 var div_1 = document.getElementById('div_botao1');
-botao1.addEventListener('mouseover', function() {
-    div_1.style.display = "flex";
-    div_1.style.flexDirection = "column";
-    div_1.style.width = "40%";    
-});
-botao1.addEventListener('mouseout', function() {
-    div_1.style.display = "none";
-    div_1.style.flexDirection = "";
-    div_1.style.width = "";  
+botao1.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (div_1.style.display === "flex") {
+        div_1.style.display = "none";
+    } else {
+        div_1.style.display = "flex";
+        div_1.style.flexDirection = "column";
+        div_1.style.width = "40%";
+    }
 });
 
+/*BOTÃO 2 */
 var div_2 = document.getElementById('div_botao2');
-botao2.addEventListener('mouseover', function() {
-    div_2.style.display = "flex";
-    div_2.style.flexDirection = "column";
-    div_2.style.width = "40%";
-});
-botao2.addEventListener('mouseout', function() {
-    div_2.style.display = "none";
+botao2.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (div_2.style.display === "flex") {
+        div_2.style.display = "none";
+    } else {
+        div_2.style.display = "flex";
+        div_2.style.flexDirection = "column";
+        div_2.style.width = "40%";
+    }
 });
 
+/*BOTÃO 3 */
 var div_3 = document.getElementById('div_botao3');
-botao3.addEventListener('mouseover', function() {
-    div_3.style.display = "flex";
-    div_3.style.flexDirection = "column";
-    div_3.style.width = "40%";
+botao3.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (div_3.style.display === "flex") {
+        div_3.style.display = "none";
+    } else {
+        div_3.style.display = "flex";
+        div_3.style.flexDirection = "column";
+        div_3.style.width = "40%";
+    }
 });
 
-botao3.addEventListener('mouseout', function() {
-    div_3.style.display = "none";
-    div_3.style.flexDirection = "";
-    div_3.style.width = "";
-});
+/*BOTÃO 4 */
 var div_4 = document.getElementById('div_botao4');
-botao4.addEventListener('mouseover', function() {
-    div_4.style.display = "flex";
-    div_4.style.flexDirection = "column";
-    div_4.style.width = "40%";
+botao4.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (div_4.style.display === "flex") {
+        div_4.style.display = "none";
+    } else {
+        div_4.style.display = "flex";
+        div_4.style.flexDirection = "column";
+        div_4.style.width = "40%";
+    }
 });
-
-botao4.addEventListener('mouseout', function() {
-    div_4.style.display = "none";
-});
-
+/*BOTÃO 5 */
 var div_5 = document.getElementById('div_botao5');
-botao5.addEventListener('mouseover', function() {
-    div_5.style.display = "flex";
-    div_5.style.flexDirection = "column";
-    div_5.style.width = "40%";
+botao5.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (div_5.style.display === "flex") {
+        div_5.style.display = "none";
+    } else {
+        div_5.style.display = "flex";
+        div_5.style.flexDirection = "column";
+        div_5.style.width = "40%";
+    }
 });
-botao5.addEventListener('mouseout', function() {
-    div_5.style.display = "none";
-});
-
+/*BOTÃO 6 */
 var div_6 = document.getElementById('div_botao6');
-botao6.addEventListener('mouseover', function() {
-    div_6.style.display = "flex";
-    div_6.style.flexDirection = "column";
-    div_6.style.width = "40%";
-});
-botao6.addEventListener('mouseout', function() {
-    div_6.style.display = "none";
+botao6.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (div_6.style.display === "flex") {
+        div_6.style.display = "none";
+    } else {
+        div_6.style.display = "flex";
+        div_6.style.flexDirection = "column";
+        div_6.style.width = "40%";
+    }
 });
